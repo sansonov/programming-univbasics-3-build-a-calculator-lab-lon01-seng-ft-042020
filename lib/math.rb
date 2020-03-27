0813446696cfe75c62ac94b3d7f8b1bea71ff3ab
Adao Natalino <sansonov@gmail.com>
2020-03-27 22:31:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ return sum
 end
 
 def subtraction(num1, num2)
-
+subs = num1 - num2
+return subs
 end
 
 def division(num1, num2)
-
+divs = num2 / num1
+return divs
 end
 
 def multiplication(num1, num2)
-
+mul = num2*num1
+return mul
 end
 
 def modulo(num1, num2)
